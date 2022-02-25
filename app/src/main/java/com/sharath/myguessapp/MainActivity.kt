24c2a79344e80b983b962809e6943b0c98b9dbff
Sharath <mailtokgsharath@gmail.com>
2022-02-25 18:53:58 +0530

    BeGame added

--- app/src/main/java/com/sharath/myguessapp/MainActivity.kt
+++ app/src/main/java/com/sharath/myguessapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.sharath.myguessapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var answer = 0
+    var isGameOver = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //generateAnswer()
+        startOver()
+    }
+    fun generateAnswer() {
+
+        answer = Random.nextInt(1,25)
+
+        val answerTextView = findViewById<TextView>(R.id.answer)
+        answerTextView.text = answer.toString()
+
+    }
+
+    fun startOver() {
+        isGameOver = false
+        generateAnswer()
+        val answerTextView = findViewById<TextView>(R.id.answer)
+        answerTextView.text = answer.toString()
+
+    }
+
+    fun btnStartOverTapped(view: View) {
+        startOver()
+    }
+
+    fun btnSubmitTapped(view: View) {
+        // generateAnswer()
+
+        isGameOver = true
+
+
+        val answerTextView = findViewById<TextView>(R.id.answer)
+        answerTextView.text = answer.toString()
+
     }
 }
